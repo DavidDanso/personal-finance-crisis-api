c9771cdaae85d63d11d04a46fa6af6148a5d1812
DavidDanso <desiingerboy@icloud.com>
2025-02-19 11:29:29 +0000

    Register UserProfile, Budget, BudgetCategory, and BudgetTransaction models in the admin interface

--- budgets/admin.py
+++ budgets/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-# from .models import Budget, BudgetCategory, BudgetTransaction
+from .models import Budget, BudgetCategory, BudgetTransaction
 
 # Register your models here.
-# admin.site.register(Budget)
-# admin.site.register(BudgetCategory)
-# admin.site.register(BudgetTransaction)
+admin.site.register(Budget)
+admin.site.register(BudgetCategory)
+admin.site.register(BudgetTransaction)
