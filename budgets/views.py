2f5360e41857c018303e586544f707e5f67b3785
DavidDanso <desiingerboy@icloud.com>
2025-02-25 11:39:32 +0000

    Remove commented-out code and clean up CreateCategoryAPIView and CreateTransactionAPIView

--- budgets/views.py
+++ budgets/views.py
@@ -1,5 +1,3 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import generics
 from .models import Budget, BudgetCategory, BudgetTransaction
 from django.shortcuts import get_object_or_404
@@ -17,7 +15,7 @@ class BudgetDetailAPIView(generics.RetrieveAPIView):
     lookup_url_kwarg = 'budget_id'
 
 
-
+#
 class CreateCategoryAPIView(generics.CreateAPIView):
     serializer_class = BudgetCategorySerializer
 
@@ -26,20 +24,13 @@ class CreateCategoryAPIView(generics.CreateAPIView):
         serializer.save(budget=budget)
 
 
-
-# POST /api/budgets/{budget_id}/categories/{category_id}/transactions/
+#
 class CreateTransactionAPIView(generics.CreateAPIView):
     serializer_class = BudgetTransactionSerializer
 
     def perform_create(self, serializer):
         budget_id = self.kwargs.get('budget_id')
         category_id = self.kwargs.get('category_id')
-
-        # First verify the budget exists and belongs to the user
         budget = get_object_or_404(Budget, id=budget_id)
-        
-        # Then verify the category exists and belongs to the budget
         category = get_object_or_404(BudgetCategory, id=category_id, budget=budget)
-        
-        # Finally save the transaction with the category
         serializer.save(category=category)
